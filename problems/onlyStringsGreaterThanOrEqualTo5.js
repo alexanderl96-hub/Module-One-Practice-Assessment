f0eb9877962cd7a7c21d9d1898676f5a78e7ed8b
Alexander <alexanderl96@yahoo.com>
2021-01-14 17:36:07 -0500

    2

--- problems/onlyStringsGreaterThanOrEqualTo5.js
+++ problems/onlyStringsGreaterThanOrEqualTo5.js
@@ -7,6 +7,19 @@
  * returns ["hello", "corey"]
  */
 
-function onlyStringsGreaterThanOrEqualTo5() {}
-
+function onlyStringsGreaterThanOrEqualTo5(words) {
+    let newString = []
+    for(let i = 0; i <= words.length; i++){
+        if(words[i].length >= words[0].length && words[i].length >= 5){
+           newString.push(words[i])
+            newString += words[i]
+            console.log(words[i])
+           //if()
+          // newString.push(words[i].length >= 5)
+           //console.log(newString)
+        }
+    }return newString
+    
+}
+console.log(onlyStringsGreaterThanOrEqualTo5(["cat", "hello", "corey", "dog"]))
 module.exports = onlyStringsGreaterThanOrEqualTo5;
