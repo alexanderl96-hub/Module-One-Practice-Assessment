51c9ca40b49814f844ed4ede0564d63c9b604e1e
Alexander <alexanderl96@yahoo.com>
2021-01-27 18:23:18 -0500

    Done

--- problems/thirdLargest.js
+++ problems/thirdLargest.js
@@ -9,8 +9,8 @@
 
 function thirdLargest(nums) {
     let largest = -Infinity
-    let largest2 = -Infinity
-    let largest3 = -Infinity
+    let secondLargest = -Infinity
+    let thirdLargest = -Infinity
 
     if(nums.length < 3 ){
         return null
@@ -18,52 +18,16 @@ function thirdLargest(nums) {
     for (let i = 0; i < nums.length; i++) {
          const num = nums[i]
          if(num > largest) {
-            largest3 = largest
-            largest2 = largest;
+            thirdLargest = secondLargest
+            secondLargest = largest;
             largest = num
-            //console.log(largest)
-       } else if (num > largest2 && num < largest) {
-         largest3 = largest2
-         largest2 = num;
-         //console.log(largest2)
-       } else if (num > largest3 && num < largest2) {
-        largest3 = num;
-        console.log(largest3)
+       } else if (num > secondLargest && num < largest) {
+         thirdLargest = secondLargest
+         secondLargest = num;
+       } else if (num > thirdLargest && num < secondLargest) {
+        thirdLargest = num;
        }
     }
-    //return largest3
+    return thirdLargest
 }
-
-console.log(thirdLargest([12, 3, 8, 2, 1, 14]))
 module.exports = thirdLargest
-
-//   for (let i = 0; i < nums.length; i++){
-//     const num = nums[i]
-//       if(nums[0] < nums[i]){
-//           largest3 = largest2
-//           largest2 = largest
-//           largest = num
-//           console.log(largest)
-//         } else if(nums[0] < nums.length-1 && nums [0]> 10){
-//             largest2 = nums[i]
-//             console.log(largest2)
-//         }
-//         // else if(num > largest3 && num < largest2){
-//         //     largest3 = num
-//         // }
-       
-//     }
-//     // return largest3
-
-
-//       if(nums.length < 3){
-//           return null
-//       }else if(nums[0] < nums[i]) {
-//           largest = nums[i]
-//           console.log(largest)
-//          }else if(nums[i] < largest) {
-//             largest2 = nums[i] 
-//             console.log(largest2) 
-//          }
-//   }
-//   return largest3
